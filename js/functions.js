ad91514081ef77cce1edf36a0f14e7088a089990
Ilya Shishlyannikov <ishishlyannikov@gmail.com>
2023-02-28 01:58:01 +0300

    feature: function 4

--- js/functions.js
+++ js/functions.js
@@ -29,3 +29,14 @@ const numberCheck = (string) => {
   return emptyString;
 };
 numberCheck('1.5');
+
+//Функция, которая принимает три параметра: исходную строку, минимальную длину и строку с добавочными символами — и возвращает исходную строку,
+//дополненную указанными символами до заданной длины. Символы добавляются в начало строки.
+//Если исходная строка превышает заданную длину, она не должна обрезаться. Если «добивка» слишком длинная, она обрезается с конца.
+const addSymbols = (string, minLength, addedSymbols) => {
+  while (string.length < minLength) {
+    string = addedSymbols.slice (0, minLength - string.length) + string;
+  }
+  return string;
+};
+addSymbols('qwerty', 4, '0');
