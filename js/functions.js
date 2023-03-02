6b48d8fe04de31f37e50873fc12698e1cba662e4
Ilya Shishlyannikov <ishishlyannikov@gmail.com>
2023-03-02 22:50:04 +0300

    feature: работа над ошибками

--- js/functions.js
+++ js/functions.js
@@ -1,40 +1,40 @@
-//Функция для проверки длины строки. Она принимает строку, которую нужно проверить, и максимальную длину и возвращает true,
-//если строка меньше или равна указанной длине, и false, если строка длиннее
-const stringCheck = (string, maxSymbols) => string.length <= maxSymbols;
-stringCheck('yesterday',5);
+//Функция для проверки длины строки.
+const checkLength = (string, maxSymbols) => string.length <= maxSymbols;
+checkLength('yesterday',5);
 
-//Функция для проверки, является ли строка палиндромом. Палиндром — это слово или фраза, которые одинаково читаются и слева направо и справа налево.
-const palindromeCheck = (string) => {
+//Функция для проверки, является ли строка палиндромом.
+const checkPalindrome = (string) => {
   const stringRegister = string.toLowerCase();
   let invertedString = '';
   for (let i = stringRegister.length - 1; i >= 0; i--) {
     if (invertedString[i] !== ' ') {
-      invertedString += stringRegister.at(i);
+      invertedString += stringRegister[i];
     }
   }
   return invertedString === stringRegister;
 };
-palindromeCheck('улыбок тебе дед Макар');
+checkPalindrome('moM');
 
 //Функция, которая принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа.
 //Если в строке нет ни одной цифры, функция должна вернуть NaN:
-const numberCheck = (string) => {
+const checkNumber = (string) => {
   let emptyString = '';
   for (let i = 0; i <= string.length; i++) {
-    if (!Number.isNaN(parseInt(string.at(i), 10))) {
-      emptyString += parseInt(string.at(i), 10);
+    const parsedString = parseInt(string[i], 10);
+    if (!Number.isNaN(parsedString)) {
+      emptyString += parsedString;
     }
   }
   return emptyString;
 };
-numberCheck('1.5');
+checkNumber('09йг3ризтий03=3+т034.2');
 
 //Функция, которая принимает три параметра: исходную строку, минимальную длину и строку с добавочными символами — и возвращает исходную строку,
 //дополненную указанными символами до заданной длины. Символы добавляются в начало строки.
 //Если исходная строка превышает заданную длину, она не должна обрезаться. Если «добивка» слишком длинная, она обрезается с конца.
 const addSymbols = (string, minLength, addedSymbols) => {
   while (string.length < minLength) {
-    string = addedSymbols.slice (0, minLength - string.length) + string;
+    string = addedSymbols.slice(0, minLength - string.length) + string;
   }
   return string;
 };
