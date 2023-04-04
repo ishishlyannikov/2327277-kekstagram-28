2c5ae6d248b99e9d29260f22d8875bf401bf7a0e
Ilya Shishlyannikov <ishishlyannikov@gmail.com>
2023-04-05 01:14:37 +0300

    feature: домашка 11

--- js/form.js
+++ js/form.js
@@ -3,6 +3,7 @@ import { increaseImageScale } from './scale.js';
 import { decreaseImageScale } from './scale.js';
 import { resetEffects } from './effects.js';
 import { initSlider } from './effects.js';
+import { sendData } from './api.js';
 
 const VALID_SYMBOLS = /^#[a-zа-яё0-9]{1,19}$/i;
 const MAX_HASHTAG_COUNT = 5;
@@ -18,6 +19,16 @@ const commentField = document.querySelector('.text__description');
 const imgPreviewContainer = document.querySelector('.img-upload__preview-container');
 const increaseImgScale = imgPreviewContainer.querySelector('.scale__control--bigger');
 const decreaseImgScale = imgPreviewContainer.querySelector('.scale__control--smaller');
+const submitButton = form.querySelector('.img-upload__submit');
+
+const successMessageTemplate = document.querySelector('#success').content.querySelector('.success');
+const errorMessageTemplate = document.querySelector('#error').content.querySelector('.error');
+const getUploadState = () => document.querySelector('.success') || document.querySelector('.error');
+
+const SubmitButtonText = {
+  IDLE: 'Опубликовать',
+  SENDING: 'Публикую...'
+};
 
 const pristine = new Pristine(form, {
   classTo: 'img-upload__field-wrapper',
@@ -26,11 +37,62 @@ const pristine = new Pristine(form, {
 }, false);
 
 const onEscape = (evt) => {
-  if (isEscapeKey (evt)) {
+  if (isEscapeKey(evt) && getUploadState() === null) {
     evt.preventDefault();
     // eslint-disable-next-line no-use-before-define
     hideModal();
   }
+};
+
+const closeMessage = () => {
+  getUploadState().remove();
+  // eslint-disable-next-line no-use-before-define
+  document.removeEventListener('keydown', onMessageEscape);
+  // eslint-disable-next-line no-use-before-define
+  document.removeEventListener('click', onOutsideElement);
+};
+
+const hideMessage = () => {
+  if(getUploadState() !== null){
+    closeMessage();
+  }
+};
+
+const onMessageEscape = (evt) =>{
+  if(isEscapeKey(evt)){
+    evt.preventDefault();
+    closeMessage();
+  }
+};
+
+const onOutsideElement = (evt) => {
+  const div = document.querySelector('.error__inner, success__inner');
+  if (evt.composedPath().includes(div)) {
+    return;
+  }
+  closeMessage();
+};
+
+const showSuccessMessage = () =>{
+  const successFragment = document.createDocumentFragment();
+  const successElement = successMessageTemplate.cloneNode(true);
+  successFragment.append(successElement);
+  document.body.append(successFragment);
+  const successButton = document.querySelector('.success__button');
+  successButton.addEventListener('click', hideMessage);
+  document.addEventListener('keydown', onMessageEscape);
+  document.addEventListener('click', onOutsideElement);
+};
+
+const showErrorMessage = () => {
+  const errorFragment = document.createDocumentFragment();
+  const errorElement = errorMessageTemplate.cloneNode(true);
+  errorFragment.append(errorElement);
+  document.body.append(errorFragment);
+  const errorButton = document.querySelector('.error__button');
+  errorButton.addEventListener('click', hideMessage);
+  document.addEventListener('keydown', onMessageEscape);
+  document.addEventListener('click', onOutsideElement);
 };
 
 const showModal = () => {
@@ -41,7 +103,7 @@ const showModal = () => {
   increaseImgScale.addEventListener('click', increaseImageScale);
 };
 
-const hideModal = () => {
+export const hideModal = () => {
   form.reset();
   pristine.reset();
   resetEffects();
@@ -79,6 +141,31 @@ const validateTags = (value) => {
 const onFormSubmit = (evt) => {
   evt.preventDefault();
   pristine.validate();
+};
+
+const blockSubmitButton = () => {
+  submitButton.disabled = true;
+  submitButton.textContent = SubmitButtonText.SENDING;
+};
+
+const unblockSubmitButton = () => {
+  submitButton.disabled = false;
+  submitButton.textContent = SubmitButtonText.IDLE;
+};
+
+export const setOnFormSubmit = () => {
+  form.addEventListener('submit', (evt) =>{
+    evt.preventDefault();
+    if(pristine.validate()){
+      blockSubmitButton();
+      sendData(new FormData(evt.target))
+        .then(showSuccessMessage)
+        .catch(() => {
+          showErrorMessage();
+        })
+        .finally(unblockSubmitButton);
+    }
+  });
 };
 
 export const initModal = () => {
--- js/utils.js
+++ js/utils.js
@@ -1,33 +1,21 @@
-export const getRandomInteger = (min, max) => {
-  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
-  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
-};
-
-export const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
-
-export const createIdGenerator = () => {
-  let lastGeneratedId = 0;
-  return () => {
-    lastGeneratedId += 1;
-    return lastGeneratedId;
-  };
-};
-
-export const createRandomIdFromRangeGenerator = (min, max) => {
-  const previousValues = [];
-  return () => {
-    let currentValue = getRandomInteger(min, max);
-    if (previousValues.length >= (max - min + 1)) {
-      return null;
-    }
-    while (previousValues.includes(currentValue)) {
-      currentValue = getRandomInteger(min, max);
-    }
-    previousValues.push(currentValue);
-    return currentValue;
-  };
-};
+const ALERT_SHOW_TIME = 5000;
 
 export const isEscapeKey = (evt) => evt.key === 'Escape';
+
+export const showAlert = (message) => {
+  const alertContainer = document.createElement('div');
+  alertContainer.style.zIndex = '100';
+  alertContainer.style.position = 'absolute';
+  alertContainer.style.left = '0';
+  alertContainer.style.top = '0';
+  alertContainer.style.right = '0';
+  alertContainer.style.padding = '30px 20px';
+  alertContainer.style.fontSize = '30px';
+  alertContainer.style.textAlign = 'center';
+  alertContainer.style.backgroundColor = 'red';
+  alertContainer.textContent = message;
+  document.body.append(alertContainer);
+  setTimeout(() => {
+    alertContainer.remove();
+  }, ALERT_SHOW_TIME);
+};
